0c661b09f9ddbd1dd904fb2437dde60f02a50c09
NaimurRahman-1998 <nrturzo1998111@gmail.com>
2023-11-24 01:08:57 +0600

    built in validation messege done

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -3,43 +3,42 @@
 import bcrypt from "bcrypt";
 import mongoose, { Schema } from "mongoose";
 import config from "../../app/config";
-import { IAddress, IFullName, IOrder, IUser, IUserModel } from "./user.interface";
+import { IUser, IUserModel } from "./user.interface";
 
-const AddressSchema = new Schema<IAddress>({
-    street: { type: String, required: true },
-    city: { type: String, required: true },
-    country: { type: String, required: true },
+const AddressSchema = new Schema({
+  street: { type: String, required: [true, 'Street is required'] },
+  city: { type: String, required: [true, 'City is required'] },
+  country: { type: String, required: [true, 'Country is required'] },
 });
 
-const OrderSchema = new Schema<IOrder>({
-    productName: { type: String, required: true },
-    price: { type: Number, required: true },
-    quantity: { type: Number, required: true },
+const OrderSchema = new Schema({
+  productName: { type: String, required: [true, 'Product name is required'] },
+  price: { type: Number, required: [true, 'Price is required'] },
+  quantity: { type: Number, required: [true, 'Quantity is required'] },
 });
 
-const FullNameSchema = new Schema<IFullName>({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
+const FullNameSchema = new Schema({
+  firstName: { type: String, required: [true, 'First name is required'] },
+  lastName: { type: String, required: [true, 'Last name is required'] },
 });
 
-const UserSchema = new Schema<IUser ,IUserModel>({
-    userId: { type: Number, required: true ,unique : true },
-    username: { type: String, required: true },
-    password: { type: String, required: true },
-    fullName: { type: FullNameSchema, required: true },
-    age: { type: Number, required: true },
-    email: { type: String, required: true },
-    isActive: { type: Boolean, required: true },
-    hobbies: { type: [String], required: true },
-    address: { type: AddressSchema, required: true },
-    orders: { type: [OrderSchema], required: true },
-    },
-    {
-        toJSON: {
-          virtuals: true,
-        },
-    }
-);
+const UserSchema = new Schema({
+  userId: { type: Number, required: [true, 'User ID is required'], unique: true },
+  username: { type: String, required: [true, 'Username is required'],},
+  password: { type: String, required: [true, 'Password is required'] },
+  fullName: { type: FullNameSchema, required: [true, 'Full name is required'] },
+  age: { type: Number, required: [true, 'Age is required']},
+  email: { type: String, required: [true, 'Email is required']},
+  isActive: { type: Boolean, required: [true, 'isActive is required'] },
+  hobbies: { type: [String], required: [true, 'Hobbies are required'] },
+  address: { type: AddressSchema, required: [true, 'Address is required'] },
+  orders: { type: [OrderSchema], required: [true, 'At least one order is required'] },
+},
+{
+  toJSON: {
+      virtuals: true,
+  },
+});
 
 
   UserSchema.pre('save', async function (next) {
