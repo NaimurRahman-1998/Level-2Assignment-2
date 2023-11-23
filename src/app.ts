4c19e83a96c405732f8c32374330f1655ba54469
NaimurRahman-1998 <nrturzo1998111@gmail.com>
2023-11-23 17:12:08 +0600

    created user Route

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 import cors from 'cors';
 import express from 'express';
+import { userRoutes } from './modules/user/user.route';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+app.use('/api/users' , userRoutes)
 
 app.get('/', (req, res) => {
-  res.send('hello');
+  res.send('welcome to assignment');
 });
 
 export default app;
---
+++ src/modules/user/user.route.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+    res.json({ message : 'Hello'})
+})
+
+export const userRoutes = router;
