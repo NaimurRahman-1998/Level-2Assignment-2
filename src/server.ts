c55db4e978b0c8c86719a0ec580938f32072d43b
NaimurRahman-1998 <nrturzo1998111@gmail.com>
2023-11-23 16:50:54 +0600

    initialized repo

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import app from './app';
+import config from './app/config';
+dotenv.config();
+
+async function main() {
+  try {
+    await mongoose.connect(config.db_url as string);
+    app.listen(config.port as string ,()=>{
+      console.log(`Server is running on port ${config.port}`);
+    })
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
